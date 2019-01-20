4aa3d0faa37e2cde3602898d961e88aeed004e6d
sensen <1107474494@qq.com>
2019-01-20 15:04:21 +0800

    biu biu biu ~

---
+++ TestLivaShare.java
@@ -0,0 +1,7 @@
+public class TestLivaShare {
+
+    public static void main(String[] args) {
+        System.out.println("hello live share");
+        System.out.println("he");
+    }
+}
--- startUp.bat
+++ startUp.bat
@@ -5,6 +5,7 @@ start D:\WXWork\WXWork.exe
 start C:\Users\TDH\Desktop\"OneNote 2013.lnk"
 start D:\NetSarang\"Xshell 6"\Xshell.exe
 start D:\VMware\"Virtual Machines"\deepin\deepin.vmx
+start E:\"Virtual Machines"\Ubuntu\Ubuntu.vmx
 start D:\Netease\CloudMusic\cloudmusic.exe
 start E:\"Program Files"\"Foxmail 7.2"\Foxmail.exe
 start C:\Users\TDH\Desktop\startNginx.bat
